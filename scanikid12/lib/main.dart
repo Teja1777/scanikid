6a75a189160cb4d815e83a526796c69dc6b226e9
Teja1777 <tejaswivakamullu1@gmail.com>
2025-08-17 10:49:32 +0530

    delete

--- scanikid12/lib/main.dart
+++ scanikid12/lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:scanikid12/pages/background.dart';
-import 'package:scanikid12/pages/base.dart';
-import 'package:scanikid12/pages/field.dart';
-import 'package:scanikid12/pages/first_page.dart';
-import 'package:scanikid12/pages/kid.dart';
+
 import 'package:scanikid12/pages/login.dart';
-import 'package:scanikid12/pages/second_page.dart';
-import 'package:scanikid12/pages/settings.dart';
-import 'package:scanikid12/pages/list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,14 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Loginpage(),
       routes: {
-        '/first': (context) => FirstPage(),
-        '/second': (context) => SecondPage(),
-        '/settings': (context) => Settings(),
-        '/background': (context) => Background(),
-        '/field': (context) => const MyWidget(),
-        '/base': (context) => const Base(),
-        '/list': (context) => const ListPage(),
-        '/kid': (context) => const Kid(),
+        
        '/login': (context) => const Loginpage(),
       },
     );
