82bc1581d8636b30a8c0a36a1625dcc5116a9ed3
Teja1777 <tejaswivakamullu1@gmail.com>
2025-09-06 10:25:56 +0530

    te

--- scanikid12/lib/pages/splash_screen.dart
+++ scanikid12/lib/pages/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:lottie/lottie.dart';
+
 import 'package:scanikid12/pages/auth_wrapper.dart';
 
 class SplashScreen extends StatelessWidget {
